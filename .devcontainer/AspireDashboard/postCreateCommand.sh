3cdbdc74dffe10a429970a1afeeb9022f0a30d63
Bruno Capuano <bcapuano@gmail.com>
2024-06-24 10:17:31 -0400

    update devcontainer desc

--- .devcontainer/AspireDashboard/postCreateCommand.sh
+++
@@ -1,8 +0,0 @@
-# update dotnet
-dotnet workload update & sudo dotnet workload install aspire & sudo dotnet workload list
-
-# Run Aspire Dashboard
-docker run --rm -it -p 18888:18888 -p 4317:18889 -d --name aspire-dashboard mcr.microsoft.com/dotnet/aspire-dashboard:8.0.0
-
-# get Phi-3
-ollama run phi3
